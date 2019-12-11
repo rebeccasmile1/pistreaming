feff698a43aed38680a61878e776c4a32ec9df4e
rebeccasmile1 <cyy201801>
2019-12-11 14:45:09 +0800

    "use ajax to send http request"

--- server.py
+++ server.py
@@ -333,7 +333,7 @@ def main():
             broadcast_thread.start()
             while True:
                 camera.wait_recording(1)
-                requestArgs=request.values()
+                requestArgs=request.args
                 func=requestArgs.get('func')
                 if func=='pylepton_capture':
                     pylepton_capture()
